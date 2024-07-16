18836fcabfae6adaa5f490bf7797a75742e7e348
coder <cybersnic@gmail.com>
2024-07-16 22:06:56 +0500

    test

--- main.py
+++ main.py
@@ -1,32 +1,20 @@
-# from fastapi import FastAPI, Request
-# from transformers import LlamaForCausalLM, AutoTokenizer
-# import torch
-#
-# # Model and tokenizer loading
-# model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
-# model = LlamaForCausalLM.from_pretrained(model_name, token='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')
-# tokenizer = AutoTokenizer.from_pretrained(model_name, token='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')
-#
-# # Use CPU instead of GPU
-# device = torch.device("cpu")
-# model.to(device)
-#
-# app = FastAPI()
-#
-#
-# @app.get("/generate")
-# async def generate(prompt: str):
-#     inputs = tokenizer.encode(prompt, return_tensors='pt').to(device)
-#     outputs = model.generate(inputs, max_length=100, num_return_sequences=1)
-#     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-#     return {"response": response}
+from fastapi import FastAPI, Request
+from transformers import LlamaForCausalLM, AutoTokenizer
+import torch
 
+model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
+model = LlamaForCausalLM.from_pretrained(model_name, token='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')
+tokenizer = AutoTokenizer.from_pretrained(model_name, token='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')
 
-from fastapi import FastAPI
+device = torch.device("cpu")
+model.to(device)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
+@app.get("/generate")
+async def generate(prompt: str):
+    inputs = tokenizer.encode(prompt, return_tensors='pt').to(device)
+    outputs = model.generate(inputs, max_length=100, num_return_sequences=1)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return {"response": response}
